4d54ebde72c66cd6bee7f7a84ea5f4b76b8d61b4
meta <eigenform@users.noreply.github.com>
2023-04-21 04:08:46 -0500

    start actually parsing things, grr

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,14 @@ use std::sync::*;
 use std::rc::*;
 use regex::*;
 
-
+/// A suitably-trimmed "line" (without comments) in some [FirrtlFile].
+#[derive(Debug)]
 pub struct FirrtlLine<'a> {
     /// The line number in the original source file
     line_number: usize,
     /// Index of the first relevant character in the original source file
     line_start: usize,
-    /// Contents
+    /// Contents (hopefully meaningful tokens)
     line: &'a str,
 }
 impl <'a> FirrtlLine<'a> {
@@ -27,8 +28,9 @@ impl <'a> FirrtlLine<'a> {
     }
 }
 
+/// Representing input from a FIRRTL source file. 
 pub struct FirrtlFile<'a> {
-    /// Set of lines from some FIRRTL source file
+    /// Set of effective lines
     pub lines: Vec<FirrtlLine<'a>>,
 }
 impl <'a> FirrtlFile<'a> {
@@ -124,29 +126,71 @@ pub enum Token<'a> {
     #[token(">")]  Greater,
     #[token("=")]  Equal,
     #[token("=>")] EqualGreater,
-
-
+}
+impl <'a> Token<'a> {
+    pub fn punctuation_from_str(s: &str) -> Self { 
+        match s {
+            "."  => Self::Period,
+            ":"  => Self::Colon,
+            "?"  => Self::Question,
+            "("  => Self::LParen,
+            ")"  => Self::RParen,
+            "{"  => Self::LBrace,
+            "}"  => Self::RBrace,
+            "["  => Self::LSquare,
+            "]"  => Self::RSquare,
+            "<"  => Self::Less,
+            "<-" => Self::LessMinus,
+            "<=" => Self::LessEqual,
+            ">"  => Self::Greater,
+            "="  => Self::Equal,
+            "=>" => Self::EqualGreater,
+            _ => panic!("Cannot convert '{}' into Token?", s),
+        }
+    }
 }
 
+/// A "tokenized" line in some [FirrtlFile], with optional FIRRTL file info.
+#[derive(Debug)]
+pub struct FirrtlTokenizedLine<'a> {
+    tokens: Vec<Token<'a>>,
+    info: Option<&'a str>,
+}
+
+/// State used for lexing/parsing a [FirrtlFile].
 pub struct FirrtlParser<'a> {
     /// Associated source file
     sf: FirrtlFile<'a>,
-    /// Index into the [FirrtlFile]
-    sf_cursor: usize,
 
+    /// Tokenized lines.
+    ///
+    /// These should be in one-to-one correspondence with [FirrtlLine]
+    /// in the associated [FirrtlFile].
+    token_lines: Vec<FirrtlTokenizedLine<'a>>,
+
+    /// "Global cursor" (index into 'token_lines')
+    gcur: usize,
+    /// "Local cursor" (index into 'token_lines[tlcur].tokens')
+    lcur:  usize,
 }
 impl <'a> FirrtlParser<'a> {
     pub fn new(sf: FirrtlFile<'a>) -> Self { 
         Self {
             sf,
-            sf_cursor: 0,
+            token_lines: Vec::new(),
+            gcur: 0,
+            lcur: 0,
         }
     }
+}
 
+impl <'a> FirrtlParser<'a> {
     // It's a lot easier to deal with things line-by-line, since FIRRTL
-    // depends on indentation (and doesn't care about whitespace otherwise)
+    // depends on indentation (and doesn't care about whitespace otherwise). 
     pub fn lex(&mut self) {
         for sfl in &self.sf.lines {
+            let mut tokens = Vec::new();
+
             // File info optionally comes at the end of a line. 
             // Separate actual line content from FIRRTL file info.
             let (content, fileinfo) = if let Some(idx) = sfl.line.find('@') {
@@ -156,32 +200,95 @@ impl <'a> FirrtlParser<'a> {
             };
 
             // Extract a set of tokens from each line
-            println!("Lexing line {}, {}", sfl.line_number(), content);
+            //println!("Lexing line {}, {}", sfl.line_number(), content);
             let mut x = Token::lexer(&content);
-            let mut tokens = Vec::new();
             while let Some(t) = x.next() {
                 let line_span = x.span();
+                let start = sfl.line_start() + line_span.start;
+                let end   = sfl.line_start() + line_span.end;
+                let ln    = sfl.line_number();
                 match t {
-                    Ok(t) => {
-                        println!("{:?}", t);
-                        tokens.push(t);
-                    },
+                    Ok(t) => tokens.push(t),
                     Err(e) => {
-                        let start = sfl.line_start() + line_span.start;
-                        let end   = sfl.line_start() + line_span.end;
-                        let ln    = sfl.line_number();
-                        panic!("unknown token at {}:{}..{}", ln, start,end);
+                        println!("{:?}", e);
+                        panic!("unknown token at line {}, offset {}..{}", 
+                               ln, start,end);
                     },
                 }
             }
-            println!("{:?}", tokens);
-            println!();
+            //println!("{:?}", tokens);
+            self.token_lines.push(FirrtlTokenizedLine {
+                tokens,
+                info: fileinfo
+            });
+            //println!();
+        }
+    }
+}
 
+impl <'a> FirrtlParser<'a> {
+    /// Return the current line
+    fn current_line(&self) -> (&FirrtlTokenizedLine, &FirrtlLine) {
+        (&self.token_lines[self.gcur], &self.sf.lines[self.gcur])
+    }
+    /// Increment the local cursor, "consuming" a token.
+    fn consume_token(&mut self) {
+        self.lcur += 1;
+    }
+    /// Increment the global cursor, moving to the next line. 
+    fn consume_line(&mut self) {
+        self.gcur += 1;
+        self.lcur = 0;
+    }
+
+    /// Return the current token
+    fn token(&mut self) -> &Token<'a> {
+        &self.token_lines[self.gcur].tokens[self.lcur]
+    }
+    fn eat_keyword(&mut self, kw: &str) {
+        if *self.token() == Token::IdentKw(kw) {
+            self.consume_token();
+        } else {
+            panic!("expected keyword {}", kw);
+        }
+    }
+    fn eat_punct(&mut self, p: &str) {
+        if *self.token() == Token::punctuation_from_str(p) {
+            self.consume_token();
+        } else {
+            panic!("expected punctuation '{}'", p);
+        }
+    }
+    fn parse_ident(&mut self) -> &'a str {
+        match *self.token() {
+            Token::IdentKw(s) => {
+                self.consume_token();
+                s
+            },
+            _ => panic!("expected ident, got {:?}", self.token()),
         }
     }
 
-}
+    pub fn parse(&mut self) {
+        // FIRRTL version
+        let (line, sfl) = self.current_line();
+        println!("Parsing {:?}", sfl);
+        println!("{:?}", line.tokens);
+        self.eat_keyword("FIRRTL");
+        self.eat_keyword("version");
+        self.consume_line();
 
+        // Circuit declaration
+        let (line, sfl) = self.current_line();
+        println!("Parsing {:?}", sfl);
+        println!("{:?}", line.tokens);
+        self.eat_keyword("circuit");
+        let circuit_id = self.parse_ident();
+        self.eat_punct(":");
+        self.consume_line();
+
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -190,6 +297,7 @@ mod tests {
     fn it_works() {
         use std::fs::File;
         use std::io::*;
+
         let filename = "./parse-basic.fir";
         let mut f = File::open(filename).unwrap();
         let mut s = String::new();
@@ -197,6 +305,7 @@ mod tests {
         let sf    = FirrtlFile::new(filename, &s);
         let mut p = FirrtlParser::new(sf);
         p.lex();
+        p.parse();
     }
 }
 
